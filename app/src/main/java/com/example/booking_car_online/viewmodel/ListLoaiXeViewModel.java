cff53792a5b8fa5a2987780e1ebebebdef0f8543
kimtanls <tanntbs@gmail.com>
2024-12-01 21:18:14 +0700

    add loaixe

--- app/src/main/java/com/example/booking_car_online/viewmodel/ListLoaiXeViewModel.java
+++ app/src/main/java/com/example/booking_car_online/viewmodel/ListLoaiXeViewModel.java
@@ -1,4 +1,25 @@
 package com.example.booking_car_online.viewmodel;
 
-public class ListLoaiXeViewModel {
+import android.content.Context;
+
+import androidx.databinding.BaseObservable;
+
+import com.example.booking_car_online.adapter.LoaiXeAdapter;
+import com.example.booking_car_online.config.AppDatabase;
+
+public class ListLoaiXeViewModel extends BaseObservable {
+    private LoaiXeAdapter loaiXeAdapter;
+    public void renderAdapter(Context context){
+        loaiXeAdapter = new LoaiXeAdapter(context);
+        loaiXeAdapter.setData(AppDatabase.getInstance(context).getLoaiXeDAO().getAll());
+        this.setLoaiXeAdapter(loaiXeAdapter);
+    }
+
+    public LoaiXeAdapter getLoaiXeAdapter() {
+        return loaiXeAdapter;
+    }
+
+    public void setLoaiXeAdapter(LoaiXeAdapter loaiXeAdapter) {
+        this.loaiXeAdapter = loaiXeAdapter;
+    }
 }
